fe7b1f47c2b241c783f7ce00a9db1890bdcb4a59
Wildan Fauzy <me@wildanfauzy.com>
2024-03-24 23:09:20 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,9 +30,9 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
-        ctaText={'shop now'}
+        title={'Selamat Datang di BrekStore'}
+        subtitle={'Melayani Segala Kebutuhan Anda'}
+        ctaText={'Order Now'}
         ctaAction={goToShop}
       />
 
